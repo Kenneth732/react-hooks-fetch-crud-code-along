28f2392c228ff34b5ea5885b7fc47c907638b1f7
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-01-27 09:42:20 +0300

    done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -7,10 +7,20 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
+
+  useEffect(() =>{
+    fetch("")
+    .then((r) => r.json())
+    .then((item) => setItems(item))
+  },[])
+  // add this function
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
-
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
